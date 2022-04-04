ecb222bcc2a55ffb43d1a2443343ae6c99985d23
SamuelKelechi <kcfreshie@ymail.com>
2022-04-04 17:41:00 +0100

    Image Upload Handler

--- src/Base.js
+++ src/Base.js
@@ -1,14 +1,14 @@
 import { initializeApp } from "firebase/app";
 import {getFirestore} from "@firebase/firestore";
-import {getStorage, getstorage} from "firebase/storage"
+import {getStorage} from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBjcljKrIaxTY2vsLE4OpJIK3yHvSPeHqs",
-    authDomain: "lsetf-database.firebaseapp.com",
-    projectId: "lsetf-database",
-    storageBucket: "lsetf-database.appspot.com",
-    messagingSenderId: "688517096125",
-    appId: "1:688517096125:web:7c72380e77f55309c74dd4"
+  apiKey: "AIzaSyBjcljKrIaxTY2vsLE4OpJIK3yHvSPeHqs",
+  authDomain: "lsetf-database.firebaseapp.com",
+  projectId: "lsetf-database",
+  storageBucket: "lsetf-database.appspot.com",
+  messagingSenderId: "688517096125",
+  appId: "1:688517096125:web:7c72380e77f55309c74dd4"
   };
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
--- src/Components/Home/Database.js
+++ src/Components/Home/Database.js
@@ -2,26 +2,61 @@ import React,{useState, useEffect} from 'react';
 import styled from 'styled-components';
 import {db} from '../../Base';
 import './Database.css';
+import moment from 'moment';
+import { storage } from '../../Base';
+import {ref, uploadBytesResumable, getDownloadURL} from 'firebase/storage';
 import {collection, getDocs, addDoc, doc, deleteDoc, Timestamp, query, orderBy, limit} from 'firebase/firestore';
+import { toast } from 'react-toastify';
 
 const Database = () => {
     const [newname, setNewname] = useState('');
     const [newage, setNewage] = useState(0);
+    const [imgurl, setImgurl] = useState('');
+    const [progress, setProgress] = useState('');
     const [newtime, setNewtime] = useState(Timestamp.now().toDate())
     const [lsetf, setLsetf] = useState([]);
 
     const usersCollectionRef = collection(db, "students")
 
-    const que = query(usersCollectionRef, orderBy("time", "desc"), limit(4))
+    const que = query(usersCollectionRef, orderBy("time", "desc"))
 
-    const AddStud = async () => {
-        await addDoc(usersCollectionRef, {name:newname, age:newage, time:newtime});
+    const AddStud = () => {
+        const storageRef = ref(storage, `/images/${Date.now()}${imgurl.name}`);
+        const uploadImage = uploadBytesResumable(storageRef, imgurl)
+        uploadImage.on("state_changed", (snapshot) => {
+        const progressPercent = Math.round(
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+        );
+        setProgress(progressPercent);
+    },
+    (err)=>{
+        console.log(err)
+    },
+    ()=> {
+        getDownloadURL(uploadImage.snapshot.ref)
+        .then((url)=> {
+            addDoc(usersCollectionRef, {name:newname, age:newage, time:newtime, imgurl:url,})
+            .then(() => {
+                toast("Uploaded Successfully", {type:"success"});
+                setProgress(0);
+            })
+            .catch(err=>{
+                toast("Upload Failed", {type:"error"});
+            })
+        })
+     
+       
+    }
+        )
+        setNewname('');
+        setNewage('');
+        setImgurl('');   
     }
 
-    const Remove = () => {
-        document.getElementById("text1").value = " ";
-        document.getElementById("text2").value = " ";
-    }
+    // const Remove = () => {
+    //     document.getElementById("text1").value = " ";
+    //     document.getElementById("text2").value = " ";
+    // }
 
     const getData = async () => {
         const data = await getDocs(que, usersCollectionRef)
@@ -40,26 +75,40 @@ const Database = () => {
 
   return (
     <Container>
-        <input id='text1' name="newname" onChange={((event) => {
+        <input  name="newname" onChange={((event) => {
             setNewname(event.target.value)
         })}  placeholder='name'/>
 
-        <input id='text2' name="newage"  onChange={((event) => {
+        <input  name="newage"  onChange={((event) => {
             setNewage(event.target.value)
         })}placeholder='age'/>
 
+        <input type="file" accept='imgurl/*' name="imgurl" onChange={((event) => {
+            setImgurl(event.target.files)
+        })} />
+
+                {
+                progress === 0 ? null: (
+                    <p style={{width:`${progress}%`}}> {`${progress}`} </p>
+                )
+                }
+
         <button  onClick={(() => {
              AddStud();
-             Remove(); 
+             console.log('Am clicked')
         })}
         >Add Student</button>
         
-
-       {lsetf.map(({id, name, age, time}) => (
+        {/* <div>{lsetf().length}</div> */}
+       {lsetf.map(({id, name, age, time, imgurl}) => (
            <div key={id}>
            <h2>Name:{name}</h2>
            <p>Age:{age}</p>
-            <p>{time.toDate().toDateString()}</p>
+            <img src={imgurl} alt="img"/>
+           {/* Syntax/Format for Momentjs */}
+           <p>  {moment(time.toDate()).fromNow()} </p>
+
+            {/* <p>{time.toDate().toDateString()}</p> */}
            <button onClick={(() => {
                DeleteStud(id)
            })}>Delete</button>
